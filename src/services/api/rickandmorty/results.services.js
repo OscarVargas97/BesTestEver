20d84a4424fb0f86381f2bb4975a6dd2510c4e7f
OscarVargas97 <oscarvargas97@gmail.com>
2022-08-15 21:33:50 -0400

    update results.services

--- src/services/api/rickandmorty/results.services.js
+++ src/services/api/rickandmorty/results.services.js
@@ -2,30 +2,38 @@ const CountCharInPropObject = require('../../general/count.services')
 const GetLocationForCharacter = require('./getlocationcorcharacter.services')
 
 function ResultFirst (resultquery, arrayinfo) {
-  return arrayinfo
-    .map(item => {
-      return {
-        char: item.resource.substring(0, 1),
-        count: CountCharInPropObject(
-          resultquery[item.resource],
-          item.resource.substring(0, 1),
-          'name'),
-        resource: item.resource
-      }
-    })
+  try {
+    return arrayinfo
+      .map(item => {
+        return {
+          char: item.resource.substring(0, 1),
+          count: CountCharInPropObject(
+            resultquery[item.resource],
+            item.resource.substring(0, 1),
+            'name'),
+          resource: item.resource
+        }
+      })
+  } catch {
+    return false
+  }
 }
 
 function ResultSecond (resultquery) {
-  return resultquery.episode.map(item => {
-    return {
-      name: item.name,
-      episode: item.episode,
-      locations: GetLocationForCharacter(
-        item.characters,
-        resultquery.character
-      )
-    }
-  })
+  try {
+    return resultquery.episode.map(item => {
+      return {
+        name: item.name,
+        episode: item.episode,
+        locations: GetLocationForCharacter(
+          item.characters,
+          resultquery.character
+        )
+      }
+    })
+  } catch {
+    return false
+  }
 }
 
 module.exports = {
